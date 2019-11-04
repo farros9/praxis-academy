5c64bae055fafd1d5667df99424336080b824b38
farros9 <farros.alfatih@gmail.com>
2019-11-04 16:31:40 +0700

    Push latihan 11-14 hari 1

---
+++ minggu-1/day-1/13/RelationalDemo.java
@@ -0,0 +1,18 @@
+public class RelationalDemo {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        boolean result = x == y;
+        System.out.println("x == y? " + result);
+        result = x != y;
+        System.out.println("x != y? " + result);
+        result = x > y;
+        System.out.println("x > y? " + result);
+        result = x >= y;
+        System.out.println("x >= y? " + result);
+        result = x < y;
+        System.out.println("x < y? " + result);
+        result = x <= y;
+        System.out.println("x <= y? " + result);
+    }
+}
