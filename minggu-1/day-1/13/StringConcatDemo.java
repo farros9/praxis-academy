f8b777ea74418cae7ae505492369a22bf56ea868
farros9 <farros.alfatih@gmail.com>
2019-11-08 10:22:50 +0700

    Push hari ke-5

--- minggu-1/day-1/13/StringConcatDemo.java
+++
@@ -1,9 +0,0 @@
-public class StringConcatDemo {
-    public static void main(String[] args) {
-        String firstName = "James";
-        String lastName = "Gosling";
-        String greeting = "Hello " + firstName + " " + lastName;
- 
-        System.out.println(greeting);
-    }
-}
